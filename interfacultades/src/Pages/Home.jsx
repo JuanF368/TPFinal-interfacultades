2cf0d98a3b333c4d2cbe8cda2fecf7859230eba5
JuanF368 <juanfarroni1314@gmail.com>
2025-09-29 21:12:46 -0300

    cambios en el comportamiento de notificacion de Home

--- interfacultades/src/Pages/Home.jsx
+++ interfacultades/src/Pages/Home.jsx
@@ -1,30 +1,44 @@
 import React, {useEffect, useState} from 'react';
-import { useLocation } from 'react-router';
+import { useLocation, useNavigate } from 'react-router';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
 const Home = () => {
   const location = useLocation();
+  const navigate = useNavigate();
+
   const [mensaje, setMensaje] = useState(''); 
+  const [visible, setVisible] = useState(false);
+
   useEffect(() => {
     if (location.state?.mensaje) {
       setMensaje(location.state.mensaje);
+      setVisible(true);
+
+      const timerFade = setTimeout(() =>{
+        setVisible(false);
+      }, 4000);
+
       const timer = setTimeout(() => {
         setMensaje('');
+        navigate(location.pathname, { replace: true });
       }, 5000); 
-      return () => clearTimeout(timer);
+      return () => {
+        clearTimeout(timer);
+        clearTimeout(timerFade);
+      };
     }
-  }, [location.state]);
+  }, [location, navigate]);
 
   return (
     <div className='flex flex-col min-h-screen'>
       <Header />
-      <main className='flex-1 container mx-auto px-4 py-6'>
-        {mensaje && (
-          <div className="mb-4 bg-green-100 text-green-800 px-4 py-2 rounded shadow transition-opacity duration-300">
+      {mensaje && (
+          <div className={`mb-4 bg-green-100 text-green-800 px-4 py-2 rounded shadow transition-opacity duration-300 ${visible ? 'opacity-100' : 'opacity-0'}`}>
             {mensaje}
           </div>
         )}
+      <main className='flex-1 container mx-auto px-4 py-6'>
         <h1 className="text-4xl font-bold">Página Principal</h1>
         <p className="mt-4 text-lg">Esta es la página principal de nuestra aplicación.</p>
       </main>
--- interfacultades/src/components/Header.jsx
+++ interfacultades/src/components/Header.jsx
@@ -29,7 +29,7 @@ const Header = () => {
                         <>
                         <button
                             onClick={handleLogout}
-                            className="bg-red-500 hover:bg-red-600 text-white px-4 py-2 rounded"
+                            className="border border-[red] hover:bg-[#8f3554] text-white px-4 py-2 rounded cursor-pointer"
                         >
                             Cerrar Sesión
                         </button>
