ebe2b8454f7a8ddbab489cafa5f2fe0d29b1adad
JuanF368 <juanfarroni1314@gmail.com>
2025-10-13 16:04:33 -0300

    agregado el resto del reglamento por archivo json

--- interfacultades/src/Pages/Reglamentos.jsx
+++ interfacultades/src/Pages/Reglamentos.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import reglamento from '../data/reglamentoGeneral.json';
 
 const Reglamentos = () => {
     const [disciplinas, setDisciplinas] = useState([]);
@@ -19,23 +20,44 @@ const Reglamentos = () => {
 
 
     return (
-        <div className="p-6">
+        <div className="p-6 text-gray-800 leading-relaxed">
             <h1 className="text-3xl font-bold mb-6 text-center">Reglamentos oficiales</h1>
-            <h3 className="text-gray-500">Juegos Interfacultades UNCo {new Date().getFullYear()}</h3>
+            <h3 className="text-gray-500 mb-20 text-center">Juegos Interfacultades UNCo {new Date().getFullYear()}</h3>
+            {reglamento.map((seccion, i) => (
+                <section key={i} className="mb-10 text-left">
+                    <h2 className="text-2xl font-semibold mb-2 text-[#bd3f15]">
+                        {seccion.titulo}
+                    </h2>
+                    {seccion.contenido.map((parrafo, j) =>(
+                        <p key={j} className="mb-2" dangerouslySetInnerHTML={{ __html: parrafo }}>
+                        </p>
+                    ))}
+                    {seccion.titulo.startsWith("6.") && (
+                        <div className="mt-8">
+                            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+                                {disciplinas.length > 0 ? (
+                                    disciplinas.map((disc) => (
+                                        <div key={disc.iddisciplina} className="bg-white shadow-md rounded-2xl p-4 hover:shadow-lg transition">
+                                            <h4 className="tesxt-lg font-semibold mb-2">{disc.nombre}</h4>
+                                            <button
+                                                onClick={() => window.open(disc.reglamento, "_blank")}
+                                                className="bg-[#E94D1A] text-white px-4 py-2 rounded hover:bg-[#c74116] transition"
+                                            >
+                                                Reglamento {disc.nombre}
+                                            </button>
+                                        </div>
+                                    ))
+                                ) : (
+                                    <p className="text-gray-500 col-span-full">
+                                        Cargando disciplinas...
+                                    </p>
+                                )}
 
-            <div className="grid grid-cold-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-                {disciplinas.map((disc) => (
-                    <div key={disc.iddisciplina} className="bg-white shadow-md rounded-2xl p-4 hover:shadow-lg transition">
-                        <h2 className="text-xl font-semibold mb-2">{disc.nombre}</h2>
-                        <button
-                            onClick={() => window.open(disc.reglamento, '_blank')}
-                            className="bg-[#E94D1A] text-white px-4 py-2 rounded hover:bg-[#c74116] cursor-pointer transition"
-                        >
-                            Ver reglamento
-                        </button>
-                    </div>
-                ))}
-            </div>
+                            </div>
+                        </div>
+                    )}
+                </section>
+            ))}
         </div>
     )
 }
